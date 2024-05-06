8b2841707b01602d51c19a97f391006140811ed4
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-06 16:53:22 -0400

    Wrote four routes for handling cupcake data requests.

--- app.py
+++ app.py
@@ -13,3 +13,49 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
 app.config['SECRET_KEY'] = "secret"
 db.init_app(app)
+
+
+@app.get("api/cupcakes")
+def get_cupcakes_data():
+    """Get data on all cupcakes. Return JSON for all instances."""
+
+    q = db.select(Cupcake).order_by(Cupcake.id)
+    cupcakes = dbx(q).scalars().all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake_data(cupcake_id):
+    """Get data on a single cupcake. Return JSON."""
+
+    cupcake = db.get_or_404(Cupcake, cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """Create a cupcake with data from body of request. Respond with JSON."""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"] or None
+
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image=image
+    )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    # return tuple of (json, status)
+    return (jsonify(cupcake=serialized), 201)
