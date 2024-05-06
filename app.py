49a30caf6d2b22dc3cbf208326d68b5925a03181
Ricardo Perez <rxperez12@gmail.com>
2024-05-06 12:19:59 -0700

    finished model

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 """Flask app for Cupcakes"""
+import os
+
+from flask import Flask, redirect, render_template, flash, jsonify, url_for
+
+from models import db, dbx, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcake")
+
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+app.config['SECRET_KEY'] = "secret"
+db.init_app(app)
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+# dbx - db.session.execute
+
+
+class Cupcake(db.Model):
+    """Class for cupcake"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.mapped_column(
+        db.Integer,
+        db.Identity(),
+        primary_true=True
+    )
+
+    flavor = db.mapped_column(
+        db.String(50),
+        nullable=False
+    )
+
+    size = db.mapped_column(
+        db.String(15),
+        nullable=False
+    )
+
+    rating = db.mapped_column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.mapped_column(
+        db.String(500),
+        nullable=False,
+        default="https://tinyurl.com/demo-cupcake"
+    )
