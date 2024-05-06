70bc89575d2d73a46869a51a25ce576e7661e078
Ricardo Perez <rxperez12@gmail.com>
2024-05-06 12:27:27 -0700

    added s to cupcake

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from models import db, dbx, Cupcake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", "postgresql:///cupcake")
+    "DATABASE_URL", "postgresql:///cupcakes")
 
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
