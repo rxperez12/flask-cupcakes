e0e760c55fa5f908b0cf869d2bc114ffde42c10d
Ricardo Perez <rxperez12@gmail.com>
2024-05-06 18:05:20 -0700

    final commit!

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,24 +1,46 @@
+const $cupcakesList = document.querySelector('.cupcakes-list');
+
 
 /** Query API for data on all cupcakes and update the list on page. */
 async function getCupcakes() {
 
   const response = await fetch('/api/cupcakes');
+  const cupcakesData = await response.json();
+  console.log('CUPCAKE DATA', cupcakesData);
+  displayCupcakes(cupcakesData);
 
-  const cupcakes_data = await response.json();
-
-  displayCupcakes(cupcakes_data);
 }
 
 /** Display cupcakes on the homepage. */
-function displayCupcakes(cupcakes_data) {
+function displayCupcakes(cupcakesData) {
+  const cupcakes = cupcakesData.cupcakes;
+
+  //WHY IS IT ONLY A NUMBER?????
+  //Create element
+  for (const cupcake of cupcakes) {
+    console.log(cupcake);
+    const $cupcake = document.createElement('li');
+
+    const $cupcakeImage = document.createElement('img');
+    $cupcakeImage.src = cupcake.image_url;
+    $cupcake.appendChild($cupcakeImage);
+    const $cupcakeFlavor = document.createElement('p');
+    $cupcakeFlavor.innerHTML = cupcake.flavor;
+    $cupcake.appendChild($cupcakeFlavor);
 
 
+    // const $cupcakeImage = document.createElement('img');
+    // const $cupcakeImage = document.createElement('img');
+    $cupcakesList.appendChild($cupcake);
+  }
 
 }
 
 document.querySelector('.btn').addEventListener("submit", handleForm);
 
-function handleForm {
+function handleForm() {
 
 }
+
+getCupcakes()
 
